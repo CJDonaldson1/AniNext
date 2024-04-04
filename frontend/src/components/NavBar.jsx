3573265e2dc0f33d8efe044524bc02ee12362db2
whoever11 <cjthesedays000@gmail.com>
2024-04-04 14:19:31 -0400

    updated

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import 'Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">AniNext</h1>
+      <div className="navbar-links">
+        <NavLink activeClassName="active" to="/homepage">HomePage</NavLink>
+        <NavLink activeClassName="active" to="/upcoming-animes">Upcoming Animes</NavLink>
+        <NavLink activeClassName="active" to="/profile">Profile</NavLink>
+        <NavLink activeClassName="active" to="/reviews">Reviews</NavLink>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
