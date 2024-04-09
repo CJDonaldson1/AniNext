45bb48e3071fe20f037cd0c85ffc38d894bdff68
whoever11 <cjthesedays000@gmail.com>
2024-04-09 09:44:00 -0400

    navbar is good to go

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "./NavBar.css"
 
 export const Navbar = () => {
@@ -7,13 +8,14 @@ export const Navbar = () => {
       <div className="navbar">
         <div className="nav-bar">
           <p className="ani-next">
+            
             <span className="text-wrapper">A</span>
             <span className="span">niNext</span>
           </p>
-          <div className="NavHomepage">HomePage</div>
-          <div className="NavUpcomingAnimes">Upcoming Animes</div>
-          <div className="NavReview">Reviews</div>
-          <div className="NavProfile">Profile</div>
+          <Link to="/" className="NavHomepage">HomePage</Link>
+          <Link to="/anime-list" className="NavUpcomingAnimes">Upcoming Animes</Link>
+          <Link to="/reviews" className="NavReview">Reviews</Link>
+          <Link to="/profile" className="NavProfile">Profile</Link>
         </div>
       </div>
     </div>
