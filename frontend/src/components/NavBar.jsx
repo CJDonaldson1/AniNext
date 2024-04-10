e00ab0d042f874c093d0f99faae2e78dfe39a54c
whoever11 <cjthesedays000@gmail.com>
2024-04-09 21:57:05 -0400

    changes

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const Navbar = () => {
+  return (
+    <div className="box">
+      <div className="navbar">
+        <div className="nav-bar">
+          <p className="ani-next">
+
+            <span className="text-wrapper">A</span>
+            <span className="span">niNext</span>
+          </p>
+          <Link to="/" className="NavHomepage">HomePage</Link>
+          <Link to="/anime-list" className="NavUpcomingAnimes">Upcoming Animes</Link>
+          <Link to="/reviews" className="NavReview">Reviews</Link>
+          <Link to="/profile" className="NavProfile">Profile</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
