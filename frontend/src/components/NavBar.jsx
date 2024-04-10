0009a04a1c575ead18b44a69c076da2d45bfaf19
whoever11 <cjthesedays000@gmail.com>
2024-04-10 09:27:28 -0400

    fixing the homepage and css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,20 +4,13 @@ import "./NavBar.css"
 
 export const Navbar = () => {
   return (
-    <div className="box">
-      <div className="navbar">
-        <div className="nav-bar">
-          <p className="ani-next">
-
-            <span className="text-wrapper">A</span>
-            <span className="span">niNext</span>
-          </p>
-          <Link to="/" className="NavHomepage">HomePage</Link>
-          <Link to="/anime-list" className="NavUpcomingAnimes">Upcoming Animes</Link>
-          <Link to="/reviews" className="NavReview">Reviews</Link>
-          <Link to="/profile" className="NavProfile">Profile</Link>
-        </div>
-      </div>
+    <div className="navbar">
+      <span className="text-wrapper">A</span>
+      <span className="span">niNext</span>
+      <Link to="/" className="NavHomepage">HomePage</Link>
+      <Link to="/anime-list" className="NavUpcomingAnimes">Upcoming Anime</Link>
+      <Link to="/reviews" className="NavReview">Reviews</Link>
+      <Link to="/profile" className="NavProfile">Profile</Link>
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -33,58 +33,32 @@ const HomePage = () => {
     const nextIndex = currentAnimeIndex + 1 === animeList.length ? 0 : currentAnimeIndex + 1
     setCurrentAnimeIndex(nextIndex)
   }
-
   return (
-    <div>
-      <div className="box">
-        <div className="group">
-          <div className="overlap">
-            <img className="anime-background" alt="Anime background" src={animeList[currentAnimeIndex].image} />
-            <div className="rectangle" />
-            <p className="anime-title">{animeList[currentAnimeIndex].title}</p>
-            <div className="season">{currentAnime.studio}</div>
-            <div className="year">{currentAnime.airingDate.split("-")[0]}</div>
-            <p className="season-description"> {currentAnime.synopsis}</p>
-           
-            <div className="star-rating">
-              <span className="star-1">&#9733;</span>
-              <span className="star-2">&#9733;</span>
-              <span className="star-3">&#9733;</span>
-              <span className="star-4">&#9733;</span>
-              <span className="star-5">&#9733;</span>
+    <div className="homepage">
+      <div className="anime-display">
+        <div className="anime-info">
+          <img className="anime-background" alt="Anime background" src={currentAnime.image} />
+          <div className="anime-details">
+            <p className="anime-title">{currentAnime.title}</p>
+            <div className="anime-metadata">
+              <span className="season">{`Season ${currentAnime.season}`}</span>
+              <span className="year">{currentAnime.airingDate.split("-")[0]}</span>
             </div>
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group">
-                <div className="text-wrapper">Save to List</div>
-                <img className="zondicons-add" alt="Zondicons add" src="zondicons-add-outline-2.svg" />
-              </div>
-            </div>
-            <div className="overlap-wrapper">
-                <div className="div">
-                  <button className="text-wrapper-2" onClick={() => saveAnime(currentAnime)}>Set Reminder</button>
-                  <img className="zondicons-add-2" alt="Zondicons add" src="zondicons-add-outline.svg" />
-                </div>
-              </div>
+            <p className="season-description">{currentAnime.synopsis}</p>
+            <button className="save-to-list" onClick={() => console.log("Save to List Clicked")}>Save to List</button>
             <p className="new-episodes-release">New Episodes Air on {currentAnime.airingDate}</p>
-            <div className="rectangle-2" />
-            <img className="image" alt="Image" src={animeList[currentAnimeIndex + 1]?.image} />
-            <img className="image-2" alt="Image" src={animeList[currentAnimeIndex + 2]?.image} />
-            <img className="image-3" alt="Image" src={animeList[currentAnimeIndex + 3]?.image} />
-            <img className="image-4" alt="Image" src={animeList[currentAnimeIndex + 4]?.image} />
-            <img className="image-5" alt="Image" src={animeList[currentAnimeIndex + 5]?.image} />
-            <div className="text-wrapper-3">NEXT ANIME</div>
-            <button className="vector-5" onClick={handleNextAnime}>Next Anime</button>
-            <div>
-              <div className="genre" />
-              {animeList.slice(currentAnimeIndex + 1, currentAnimeIndex + 6).map((anime, index) => (
-                <div className={`genre-${index + 1}`} key={index}>{anime.genres[0]}</div>
-              ))}
-            </div>
           </div>
+        </div>
+        <div className="next-anime-preview">
+          <p className="text-next">NEXT ANIME</p>
+          {animeList.slice(currentAnimeIndex + 1, currentAnimeIndex + 6).map((anime, index) => (
+            <img key={index} className="next-anime-img" alt={`${anime.title} preview`} src={anime.image} />
+          ))}
+          <button className="next-anime-button" onClick={handleNextAnime}>Next Anime</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage
